f8d34e38b154db09e36b711d9890d905adc6904c
Ines CV <inescastelltort@gmail.com>
2019-07-02 18:37:08 +0200

    ADD Hooks nomenclature

--- src/index.js
+++ src/index.js
@@ -51,11 +51,11 @@ const trans = (r, s) => `perspective(1500px) rotateX(5deg) rotateY(${r / 10}deg)
 
 function Deck() {
   const [gone] = useState(() => new Set()) // The set flags all the cards that are flicked out
-  const [flipped, setFlip] = useState(false)
-  const [props, set] = useSprings(cards.length, i => ({ ...to(i), from: from(i) })) // Create a bunch of springs using the helpers above
+  const [isFlipped, setIsFlipped] = useState(false)
+  const [opCards, setOpCards] = useSprings(cards.length, i => ({ ...to(i), from: from(i) })) // Create a bunch of springs using the helpers above
   const { transform, opacity } = useSpring({
-    opacity: flipped ? 1 : 0,
-    transform: `perspective(600px) rotateY(${flipped ? 180 : 0}deg)`,
+    opacity: isFlipped ? 1 : 0,
+    transform: `perspective(600px) rotateY(${isFlipped ? 180 : 0}deg)`,
     config: { mass: 7, tension: 500, friction: 80 }
   })
   // Create a gesture, we're interested in down-state, delta (current-pos - click-pos), direction and velocity
@@ -63,7 +63,7 @@ function Deck() {
     const trigger = velocity > 0.2 // If you flick hard enough it should trigger the card to fly out
     const dir = xDir < 0 ? -1 : 1 // Direction should either point left or right
     if (!down && trigger) gone.add(index) // If button/finger's up and trigger velocity is reached, we flag the card ready to fly out
-    set(i => {
+    setOpCards(i => {
       if (index !== i) return // We're only interested in changing spring-data for the current spring
       const isGone = gone.has(index)
       const x = isGone ? (200 + window.innerWidth) * dir : down ? xDelta : 0 // When a card is gone it flys out left or right, otherwise goes back to zero
@@ -78,12 +78,12 @@ function Deck() {
       }
       return { x, rot, scale, delay: undefined, config: { friction: 50, tension: down ? 800 : isGone ? 200 : 500 } }
     })
-    if (!down && gone.size === cards.length) setTimeout(() => gone.clear() || set(i => to(i)), 600) // It makes the cards return after there are none left
+    if (!down && gone.size === cards.length) setTimeout(() => gone.clear() || setOpCards(i => to(i)), 600) // It makes the cards return after there are none left
   })
   
   // Now we're just mapping the animated values to our view, that's it. Btw, this component only renders once. :-)
-  return props.map(({ x, y, rot, scale }, i) => (
-    <animated.div key={i} style={{ transform: interpolate([x, y], (x, y) => `translate3d(${x}px,${y}px,0)`) }} onDoubleClick={() => setFlip(state => !state)}>
+  return opCards.map(({ x, y, rot, scale }, i) => (
+    <animated.div key={i} style={{ transform: interpolate([x, y], (x, y) => `translate3d(${x}px,${y}px,0)`) }} onDoubleClick={() => setIsFlipped(state => !state)}>
       {/* This is the card itself, we're binding our gesture to it (and inject its index so we know which is which) */}
         <animated.div className="cards" {...bind(i)} style={{ transform: interpolate([rot, scale], trans) }} >
             <animated.div className="card front" style={{ opacity: opacity.interpolate(o => 1 - o), transform }} >
